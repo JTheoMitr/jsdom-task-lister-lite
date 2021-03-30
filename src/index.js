fa15c3a3bf15277b28c72515df30157393dad8ad
John Mitropoulos <jmitropoulos@alumni.berklee.edu>
2021-03-30 18:27:08 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  
+  const form = document.querySelector('form');
+  let inputTag = document.querySelector('input#new-task-description');
+
+  form.addEventListener('submit', function(event){
+    event.preventDefault();
+
+    let inputValue = inputTag.value;
+
+    let newLi = document.createElement('li');
+    newLi.innerText = `${inputValue}            `;
+
+    let newButton = document.createElement('button');
+    newButton.innerText = " Delete "
+
+    let ulTag = document.querySelector('ul#tasks');
+
+      if (inputTag.value != '') {
+        ulTag.append(newLi);
+        newLi.append(newButton);
+        inputTag.value = '';
+      }
+      else {
+        inputTag.value = '';
+      }
+
+
+  })
 });
